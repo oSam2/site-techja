5eee6f0cada953f0cca72c2362580972156e9085
oSam2 <samuelmarques96@live.com>
2021-11-30 19:37:40 -0300

    cart number

---
+++ template/static/js/script.js
@@ -0,0 +1,13 @@
+// function addToCart(){
+//     document.getElementsByClassName('btn btn-primary').innerHTML = "teste"
+// }
+
+
+var count = 0;
+function addToCart() {
+    // document.getElementById('compra');
+    a = document.getElementById('cart')
+    count += 1;
+    a.innerHTML = count;
+    
+}

035824d8e74e716b33f85e1648c37dc97a3ec586
oSam2 <samuelmarques96@live.com>
2021-11-30 19:40:46 -0300

    removed comments from js file

--- template/static/js/script.js
+++ template/static/js/script.js
@@ -1,11 +1,5 @@
-// function addToCart(){
-//     document.getElementsByClassName('btn btn-primary').innerHTML = "teste"
-// }
-
-
 var count = 0;
 function addToCart() {
-    // document.getElementById('compra');
     a = document.getElementById('cart')
     count += 1;
     a.innerHTML = count;
